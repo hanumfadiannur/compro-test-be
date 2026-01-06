67bc0849a530e263ae6a70bc81e742bd0f21bd1c
hanumfadiannur <147732081+hanumfadiannur@users.noreply.github.com>
2026-01-07 05:29:39 +0700

    Configure ESLint and TypeScript to ignore build errors
    Added ESLint and TypeScript configuration options to ignore errors during builds.

--- Front-End-main/next.config.mjs
+++ Front-End-main/next.config.mjs
@@ -1,21 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com'
+        hostname: 'images.unsplash.com',
       },
       {
         protocol: 'https',
-        hostname: 'homedecorindonesia.com'
+        hostname: 'homedecorindonesia.com',
       },
       {
         protocol: 'https',
-        hostname: 'www.homedecorindonesia.com'
-      }
-    ]
-  }
+        hostname: 'www.homedecorindonesia.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
